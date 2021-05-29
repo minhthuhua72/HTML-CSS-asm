4c20748bbe0d2d2a23966512b8d45af35e3cd604
minhthuhua72 <s3878667@rmit.edu.vn>
2021-05-29 21:08:51 +0700

    update php 6

---
+++ browse-store-category.php
@@ -0,0 +1,88 @@
+<?php 
+// if(isset($_POST['category'])){
+//     $selected = $_POST['category'];
+//     switch ($selected) {
+//         case 'accessories':
+//             echo ' <div class="stores-list-cate">
+//             <div class="category">
+//                 <h2>Fashion Accessories</h2>
+//             </div>
+//             <div class="store-browse">
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/charles.png" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Charles & Keith</h3>
+//                     </a>
+//                 </div>
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/pandora.jpg" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Pandora</h3>
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>';
+//             break;
+//         case 'unisex':
+//             echo ' <div class="stores-list-cate">
+//             <div class="category">
+//                 <h2>Unisex Apparel</h2>
+//             </div>
+//             <div class="store-browse">
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/nike.png" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Nike</h3>
+//                     </a>
+//                 </div>
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/tommy.jpg" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Tommy Hilfiger</h3>
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>';
+//             break;
+//         default:
+//             echo ' <div class="stores-list-cate">
+//             <div class="category">
+//                 <h2>Fashion Accessories</h2>
+//             </div>
+//             <div class="store-browse">
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/charles.png" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Charles & Keith</h3>
+//                     </a>
+//                 </div>
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/pandora.jpg" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Pandora</h3>
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>
+
+//         <div class="stores-list-cate">
+//             <div class="category">
+//                 <h2>Unisex Apparel</h2>
+//             </div>
+//             <div class="store-browse">
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/nike.png" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Nike</h3>
+//                     </a>
+//                 </div>
+//                 <div class="stores-lst">
+//                     <a href="nike/nike.html"><img class="storeLogos" src="imgs/tommy.jpg" alt="store-logo"></a>
+//                     <a href="nike/nike.html">
+//                         <h3>Tommy Hilfiger</h3>
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>';
+//             break;
+// }}
