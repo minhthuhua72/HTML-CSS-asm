cc43d9016123d84e32cb036ba0961d226689511e
minhthuhua72 <s3878667@rmit.edu.vn>
2021-06-01 22:03:37 +0700

    update php1

--- install.php
+++ install.php
@@ -12,9 +12,7 @@
             $repass = $_POST['repass'];
             $repass_hash = password_hash($_POST ['repass'], PASSWORD_DEFAULT);
             if(empty($_POST['username'])){
-
-                $err['username']="Please enter username admin!!!";
-    
+                $err['username']="Please enter username for admin!!!";
             }
             if(empty($_POST ['pass'])){
                 $err['pass']="Please enter password!!!";
@@ -22,13 +20,16 @@
             if($_POST ['pass'] !== $_POST['repass'] ){
                 $err['repass']="Please enter correct repassword!!!";
             }
-            
-            if($nortification == ''){
-                $file_open = fopen("admin.csv", "a");
-                $no_rows = count(file("admin.csv"));          
+
+            if(empty($err)){
+                $file_open = fopen ("admin.csv", "a");
+                $no_rows = count(file("admin.csv"));
+
                 if($no_rows > 1){
                     $no_rows = ($no_rows - 1) + 1;
-                }
+                    echo '<span class="account"><p>Successful account created!</p></span>';
+                }   
+               
                 $form_data = array(
                 'sr_no'  => $no_rows,
                 'username'  => $username,
@@ -39,7 +40,6 @@
                 $username = '';
                 $pass_hash = '';
                 $repass = '';
-                echo "Successfull account !!!";
             }
         }
 
@@ -50,36 +50,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Install</title>
+    <link rel="stylesheet" href="install.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 </head>
 <body>
-<style>
-    .errPHP{
-        color: red;
-    }
-</style>
-
-    <h1>Admin</h1>
+    <h1>Install</h1>
     <form method="post">
-        <label for="">Username Admin</label>
-        <input type="text" name="username"> <br>
-        <div class="errPHP">
-            <span><?php echo (isset($err['username']))?$err['username']:'' ?></span>
-        </div>
-        
-        <label for="">Password</label>
-        <input type="text" name="pass"> <br>
-        <div class="errPHP">
-            <span><?php echo (isset($err['pass']))?$err['pass']:'' ?></span>
-        </div>
-        <label for="">Repassword</label>
-        <input type="text" name="repass"> <br>
-        <div class="errPHP">
-            <span><?php echo (isset($err['repass']))?$err['repass']:'' ?></span>
-        </div>
-
-        <input type="submit" name= "submit" value="Submit">
-
+        <fieldset>
+            <legend>For Admin</legend>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username"> <br>
+            <div class="errPHP">
+                <span><?php echo (isset($err['username']))?$err['username']:'' ?></span>
+            </div>
+            
+            <label for="pass">Password</label>
+            <input type="text" id="pass" name="pass"> <br>
+            <div class="errPHP">
+                <span><?php echo (isset($err['pass']))?$err['pass']:'' ?></span>
+            </div>
+            <label for="repass">Retype Password</label>
+            <input type="text" id="repass" name="repass"> <br>
+            <div class="errPHP">
+                <span><?php echo (isset($err['repass']))?$err['repass']:'' ?></span>
+            </div>
+            <input type="submit" name= "submit" value="Submit">
+        </fieldset>
         
     </form>
     
